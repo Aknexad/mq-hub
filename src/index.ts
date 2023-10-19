ecce00197a29e2baef198aa24270febab7bd1bd8
Aknexad <aknejad@outlook.com>
2023-10-19 08:55:12 +0330

    fix export type and main path

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { AmqpConnection } from './connection';
 import { PublishSubscribe } from './messaging-method';
+import { CreateChannelInput, PublishInput, SubscribeInput } from './types';
 
 export { AmqpConnection, PublishSubscribe };
+
+export type { CreateChannelInput, PublishInput, SubscribeInput };
