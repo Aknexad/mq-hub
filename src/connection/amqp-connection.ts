819591d125973de07fc1276c7a2e175a37dce89e
Aknexad <aknejad@outlook.com>
2024-01-15 20:28:08 +0330

    fix bug

--- src/connection/amqp-connection.ts
+++ src/connection/amqp-connection.ts
@@ -11,6 +11,7 @@ class AmqpConnection implements IAmqpConnection {
 
       return this.connectToRabbitMQ;
     } catch (error) {
+      console.log('error in connecting to rabbitmq => ' + error.message);
       throw error;
     }
   }
@@ -24,6 +25,7 @@ class AmqpConnection implements IAmqpConnection {
       this.channel.prefetch(prefetchCount);
       return this.channel;
     } catch (error) {
+      console.log('error in creating channel => ' + error.message);
       throw error;
     }
   }
@@ -44,6 +46,88 @@ class AmqpConnection implements IAmqpConnection {
         await this.channel.close();
       }
     } catch (error) {
+      throw error;
+    }
+  }
+
+  public async ReconnectionToRabbitMq(rabbitMqUrl: string, maxRetries: number = 5) {
+    let retries = 0;
+    while (retries < maxRetries) {
+      try {
+        // Code to connect to RabbitMQ server
+
+        this.connectToRabbitMQ = await amqp.connect(rabbitMqUrl);
+
+        console.log('Connected to RabbitMQ successfully!');
+        break; // Break out of the loop if the connection is successful
+      } catch (error) {
+        if (error.code === 'ECONNREFUSED') {
+          console.error('Connection to RabbitMQ refused. Retrying...');
+          retries++;
+          await new Promise(resolve => setTimeout(resolve, 5000)); // Delay before retrying
+        } else {
+          console.error('Unexpected error:', error.message);
+          break; // Break out of the loop if an unexpected error occurs
+        }
+      }
+    }
+  }
+
+  public async ReconnectToRabbitMqOnErrors(rabbitMqUrl: string) {
+    try {
+      const connection = await this.AmqpConnections(rabbitMqUrl);
+
+      // Event handler for when the connection is closed
+      connection.on('close', err => {
+        if (err) {
+          console.error(`Connection closed with error: ${err}`);
+        }
+      });
+
+      // Attempt to reconnect after a delay (e.g., 5 seconds)
+      setTimeout(() => {
+        console.log('Attempting to reconnect to RabbitMQ...');
+        this.ReconnectToRabbitMqOnErrors(rabbitMqUrl);
+      }, 5000);
+
+      // Event handler for when an error occurs
+      connection.on('error', err => {
+        console.error(`Connection error: ${err}`);
+      });
+
+      // Do further setup, like creating a channel
+      const channel = await this.CreateChannel(connection);
+      return channel;
+
+      console.log('Connected to RabbitMQ');
+    } catch (error) {
+      console.error(`Failed to connect to RabbitMQ: ${error}`);
+      // Retry the connection after a delay (e.g., 5 seconds)
+      setTimeout(() => {
+        console.log('Retrying RabbitMQ connection...');
+        this.ReconnectToRabbitMqOnErrors(rabbitMqUrl);
+      }, 5000);
+    }
+  }
+
+  public async RecreateChannel(connection: amqp.Connection) {
+    try {
+      let retries = 0;
+      const maxRetries = 5; // Maximum number of retries
+      while (retries < maxRetries) {
+        try {
+          this.channel = await this.CreateChannel(connection);
+
+          console.log('Connected to RabbitMQ successfully!');
+
+          break; // Break out of the loop if the connection is successful
+        } catch (error) {
+          console.error(`error => ${error}`);
+          retries++;
+        }
+      }
+    } catch (error) {
+      console.log(`creating channel error => ${error}`);
       throw error;
     }
   }
--- src/messaging-method/publish-subscribe.ts
+++ src/messaging-method/publish-subscribe.ts
@@ -1,37 +1,58 @@
 import amqp from 'amqplib';
 
 import { IPublisherConsumer, PublishInput, SubscribeInput } from '../types';
+import { channel } from 'diagnostics_channel';
+import { error } from 'console';
 
 class PublishSubscribe implements IPublisherConsumer {
   public async PublishMassage(data: PublishInput): Promise<void> {
-    const { channel, exchangeName, bindingKey, massage, options } = data;
+    try {
+      const { channel, exchangeName, bindingKey, massage, options } = data;
 
-    const payload = Buffer.from(JSON.stringify(massage));
+      if (!channel) {
+        console.log(`channel is close reconnect to rabbitmq `);
+      }
 
-    channel.publish(exchangeName, bindingKey, payload, options);
+      const payload = Buffer.from(JSON.stringify(massage));
 
-    return;
+      channel.publish(exchangeName, bindingKey, payload, options);
+
+      return;
+    } catch (error) {
+      console.error(`errors in publishing message =>  ${error.message}`);
+      throw error;
+    }
   }
 
   public async SubscribeMassage(data: SubscribeInput): Promise<void> {
-    const { channel, quemName, logic } = data;
+    try {
+      const { channel, quemName, logic } = data;
 
-    channel.consume(quemName, async (data: amqp.ConsumeMessage | null) => {
-      if (!data) {
-        return;
-      }
-      const payload = JSON.parse(data.content.toString());
-
-      const prosesData = await logic(payload);
-
-      if (!prosesData) {
-        return;
+      if (!channel) {
+        console.log(`channel is close reconnect to rabbitmq `);
       }
 
-      channel.ack(data);
-    });
+      channel.consume(quemName, async (data: amqp.Message | null) => {
+        try {
+          if (!data) {
+            return;
+          }
+          const payload = JSON.parse(data.content.toString());
 
-    return;
+          const prosesData = await logic(payload);
+
+          channel.ack(data);
+        } catch (error) {
+          console.error('errors in subscribing message => ' + error.message);
+          channel.reject(data!, false); // Reject the message and re-queue it for later processing
+          return; // Return early to prevent further processing of the message
+        }
+      });
+
+      return;
+    } catch (error) {
+      console.error('errors in subscribing message => ' + error.message);
+    }
   }
 }
 
